cdbc2d9dd0a6258421e2b7f28639e04409bcb5ee
Dekapi <poddajsie96@gmail.com>
2025-01-11 20:29:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    const nav = document.querySelector('.nav');
     sidebar.classList.toggle('collapsed');
-    nav.classList.toggle('hidden');
 }
